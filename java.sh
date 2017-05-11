b9ee71fb8888808af5ffbeeaebac62184a285455
Wes Richardet <wes@tetriscodes.com>
2017-05-11 18:47:47 -0500

    Updating Protobuf

--- java.sh
+++ java.sh
@@ -2,16 +2,11 @@
 node index.js java
 mkdir -p src/main/java/com/boundlessgeo/spatialconnect/schema
 mv SCCommand.java src/main/java/com/boundlessgeo/spatialconnect/schema/
-protoc SCMessage.proto --java_out="./"
+protoc APIMessage.proto --java_out="./src/main/java"
 protoc Feature.proto --java_out="./src/main/java"
-
-echo "package com.boundlessgeo.spatialconnect.schema;" > tmp
-cat SCMessageOuterClass.java >> tmp
-rm SCMessageOuterClass.java
-mv tmp src/main/java/com/boundlessgeo/spatialconnect/schema/SCMessageOuterClass.java
 
 ./gradlew build
 ./gradlew install
 ./gradlew uploadArchives
 
-rm -rf src
+#rm -rf src

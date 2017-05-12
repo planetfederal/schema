d7750728c62e9efcc953dd34d32ace2b9f3cf565
Wes Richardet <wes@tetriscodes.com>
2017-05-12 12:53:16 -0500

    Renaming API Message to Msg

--- java.sh
+++ java.sh
@@ -2,7 +2,7 @@
 node index.js java
 mkdir -p src/main/java/com/boundlessgeo/spatialconnect/schema
 mv SCCommand.java src/main/java/com/boundlessgeo/spatialconnect/schema/
-protoc APIMessage.proto --java_out="./src/main/java"
+protoc Msg.proto --java_out="./src/main/java"
 protoc Feature.proto --java_out="./src/main/java"
 
 ./gradlew build

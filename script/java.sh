ebd2738d6362f3a02e101e61b5436f1403217e6e
georific <lrobinson@boundlessgeo.com>
2017-07-14 17:04:54 -0400

    fixing java out path for protoc

--- script/java.sh
+++ script/java.sh
@@ -2,8 +2,8 @@
 node script/index.js java
 mkdir -p ./java/src/main/java/com/boundlessgeo/schema
 mv Actions.java ./java/src/main/java/com/boundlessgeo/schema/
-protoc ./proto/Msg.proto --java_out="./src/main/java"
-protoc ./proto/Feature.proto --java_out="./src/main/java"
+protoc ./proto/Msg.proto --java_out="./java/src/main/java"
+protoc ./proto/Feature.proto --java_out="./java/src/main/java"
 
 ./java/gradlew -p java build
 ./java/gradlew -p java install

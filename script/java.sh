70cfa9cb304d30ece3624a105f01c4fe1e8d8c2a
Wes Richardet <wes@tetriscodes.com>
2017-08-03 16:26:58 -0500

    Adding API Layer actions

--- script/java.sh
+++ script/java.sh
@@ -5,9 +5,6 @@ mv Actions.java ./java/src/main/java/com/boundlessgeo/schema/
 protoc ./proto/Msg.proto --java_out="./java/src/main/java"
 protoc ./proto/Feature.proto --java_out="./java/src/main/java"
 
-./java/gradlew -p java build
-./java/gradlew -p java install
-./java/gradlew -p java uploadArchives
-
-rm -rf ./java/src
-rm -rf ./java/build
+cd java
+mvn install
+cd ..

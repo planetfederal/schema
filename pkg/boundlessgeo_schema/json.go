733f5f0c35438f1d6dbe120bd0de8d771c4e9864
Josh Kendrick <joshkendrick@users.noreply.github.com>
2018-07-18 10:26:33 -0500

    cleaning up Command json

--- pkg/boundlessgeo_schema/json.go
+++ pkg/boundlessgeo_schema/json.go
@@ -6,97 +6,46 @@ package schema
 
 import "encoding/json"
 
+type aliasCommand Command
+type jsonCommand struct {
+	Data map[string]interface{} `protobuf:"message,3,opt,name=data,proto3" json:"data,omitempty"`
+	*aliasCommand
+}
+
 // MarshalJSON returns the JSON encoding of a Command
 func (command *Command) MarshalJSON() ([]byte, error) {
-	// objMap := make(map[string]interface{})
-	// objMap["id"] = command.Id
-	// objMap["action"] = command.Action
-
-	// var dataAsJSON interface{}
-	// if unmarshalErr := json.Unmarshal(command.Data, &dataAsJSON); unmarshalErr != nil {
-	// 	return nil, unmarshalErr
-	// }
-	// objMap["data"] = dataAsJSON
-
-	// objMap["context"] = command.Context
-	// objMap["metadata"] = command.Metadata
-	// objMap["created"] = command.Created
-	// return json.Marshal(objMap)
-
+	// get the data field as json
 	var dataAsJSON map[string]interface{}
 	if unmarshalErr := json.Unmarshal(command.Data, &dataAsJSON); unmarshalErr != nil {
 		return nil, unmarshalErr
 	}
 
-	type Alias Command
-	return json.Marshal(&struct {
-		Data map[string]interface{} `protobuf:"message,3,opt,name=data,proto3" json:"data,omitempty"`
-		*Alias
-	}{
-		Data:  dataAsJSON,
-		Alias: (*Alias)(command),
+	return json.Marshal(&jsonCommand{
+		Data:         dataAsJSON,
+		aliasCommand: (*aliasCommand)(command),
 	})
 }
 
 // UnmarshalJSON parses JSON-encoded data into a Command
 func (command *Command) UnmarshalJSON(bytes []byte) error {
-	// deserialize into a map
-	// var objMap map[string]*json.RawMessage
-	// err := json.Unmarshal(bytes, &objMap)
-	// if err != nil {
-	// 	return err
-	// }
-
-	// // get the command's id
-	// if err = json.Unmarshal(*objMap["id"], &command.Id); err != nil {
-	// 	return err
-	// }
-
-	// // get the command's action
-	// if err = json.Unmarshal(*objMap["action"], &command.Action); err != nil {
-	// 	return err
-	// }
-
-	// // get the command's data
-	// if err = json.Unmarshal(*objMap["data"], &command.Data); err != nil {
-	// 	return err
-	// }
-
-	// // get the command's context
-	// if err = json.Unmarshal(*objMap["context"], &command.Context); err != nil {
-	// 	return err
-	// }
-
-	// // get the command's metadata
-	// if err = json.Unmarshal(*objMap["metadata"], &command.Metadata); err != nil {
-	// 	return err
-	// }
-
-	// // get the command's created
-	// if err = json.Unmarshal(*objMap["created"], &command.Created); err != nil {
-	// 	return err
-	// }
-
-	// // no errors
-	// return nil
-
-	type Alias Command
-	aux := &struct {
-		Data map[string]interface{} `protobuf:"message,3,opt,name=data,proto3" json:"data,omitempty"`
-		*Alias
-	}{
-		Alias: (*Alias)(command),
+	// create a jsonCommand object
+	aux := &jsonCommand{
+		aliasCommand: (*aliasCommand)(command),
 	}
 
+	// read the JSON data into a jsonCommand
 	if err := json.Unmarshal(bytes, &aux); err != nil {
 		return err
 	}
 
-	dataJSON, parseErr := json.Marshal(aux.Data)
+	// convert the Data json object to bytes
+	data, parseErr := json.Marshal(aux.Data)
 	if parseErr != nil {
 		return parseErr
 	}
-	command.Data = dataJSON
 
+	command.Data = data
+
+	// no errors
 	return nil
 }

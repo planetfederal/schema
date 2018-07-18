a1cf1fa4f961c7835d2158f6b7754b7454879434
Josh Kendrick <joshkendrick@users.noreply.github.com>
2018-07-18 11:15:17 -0500

    add json handling for Events

--- pkg/boundlessgeo_schema/json.go
+++ pkg/boundlessgeo_schema/json.go
@@ -49,3 +49,47 @@ func (command *Command) UnmarshalJSON(bytes []byte) error {
 	// no errors
 	return nil
 }
+
+type aliasEvent Event
+type jsonEvent struct {
+	Data map[string]interface{} `protobuf:"message,3,opt,name=data,proto3" json:"data,omitempty"`
+	*aliasEvent
+}
+
+// MarshalJSON returns the JSON encoding of an Event
+func (event *Event) MarshalJSON() ([]byte, error) {
+	// get the data field as json
+	var dataAsJSON map[string]interface{}
+	if unmarshalErr := json.Unmarshal(event.Data, &dataAsJSON); unmarshalErr != nil {
+		return nil, unmarshalErr
+	}
+
+	return json.Marshal(&jsonEvent{
+		Data:       dataAsJSON,
+		aliasEvent: (*aliasEvent)(event),
+	})
+}
+
+// UnmarshalJSON parses JSON-encoded data into a Command
+func (event *Event) UnmarshalJSON(bytes []byte) error {
+	// create a jsonEvent object
+	aux := &jsonEvent{
+		aliasEvent: (*aliasEvent)(event),
+	}
+
+	// read the JSON data into a jsonEvent
+	if err := json.Unmarshal(bytes, &aux); err != nil {
+		return err
+	}
+
+	// convert the Data json object to bytes
+	data, parseErr := json.Marshal(aux.Data)
+	if parseErr != nil {
+		return parseErr
+	}
+
+	event.Data = data
+
+	// no errors
+	return nil
+}

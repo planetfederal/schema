b5743c87e0ffe3ef8bd5133b1a89b65dba1fe25b
Josh Kendrick <joshkendrick@users.noreply.github.com>
2018-07-18 14:44:01 -0500

    extend JSON functionality for Command, Event (#61)
    * WIP adding json helpers to golang src
    
    * attempt to preserve omitempty, similar
    
    * cleaning up Command json
    
    * add json handling for Events
    
    * prevent non-gen go sources from deletion
    
    * version bump v0.18.1

---
+++ pkg/boundlessgeo_schema/json.go
@@ -0,0 +1,95 @@
+// Copyright 2018, Boundless, https://boundlessgeo.com - All Rights Reserved
+// Unauthorized copying of this file, via any medium is strictly prohibited
+// Proprietary and confidential
+
+package schema
+
+import "encoding/json"
+
+type aliasCommand Command
+type jsonCommand struct {
+	Data map[string]interface{} `protobuf:"message,3,opt,name=data,proto3" json:"data,omitempty"`
+	*aliasCommand
+}
+
+// MarshalJSON returns the JSON encoding of a Command
+func (command *Command) MarshalJSON() ([]byte, error) {
+	// get the data field as json
+	var dataAsJSON map[string]interface{}
+	if unmarshalErr := json.Unmarshal(command.Data, &dataAsJSON); unmarshalErr != nil {
+		return nil, unmarshalErr
+	}
+
+	return json.Marshal(&jsonCommand{
+		Data:         dataAsJSON,
+		aliasCommand: (*aliasCommand)(command),
+	})
+}
+
+// UnmarshalJSON parses JSON-encoded data into a Command
+func (command *Command) UnmarshalJSON(bytes []byte) error {
+	// create a jsonCommand object
+	aux := &jsonCommand{
+		aliasCommand: (*aliasCommand)(command),
+	}
+
+	// read the JSON data into a jsonCommand
+	if err := json.Unmarshal(bytes, &aux); err != nil {
+		return err
+	}
+
+	// convert the Data json object to bytes
+	data, parseErr := json.Marshal(aux.Data)
+	if parseErr != nil {
+		return parseErr
+	}
+
+	command.Data = data
+
+	// no errors
+	return nil
+}
+
+type aliasEvent Event
+type jsonEvent struct {
+	Data map[string]interface{} `protobuf:"message,3,opt,name=data,proto3" json:"data,omitempty"`
+	*aliasEvent
+}
+
+// MarshalJSON returns the JSON encoding of an Event
+func (event *Event) MarshalJSON() ([]byte, error) {
+	// get the data field as json
+	var dataAsJSON map[string]interface{}
+	if unmarshalErr := json.Unmarshal(event.Data, &dataAsJSON); unmarshalErr != nil {
+		return nil, unmarshalErr
+	}
+
+	return json.Marshal(&jsonEvent{
+		Data:       dataAsJSON,
+		aliasEvent: (*aliasEvent)(event),
+	})
+}
+
+// UnmarshalJSON parses JSON-encoded data into a Command
+func (event *Event) UnmarshalJSON(bytes []byte) error {
+	// create a jsonEvent object
+	aux := &jsonEvent{
+		aliasEvent: (*aliasEvent)(event),
+	}
+
+	// read the JSON data into a jsonEvent
+	if err := json.Unmarshal(bytes, &aux); err != nil {
+		return err
+	}
+
+	// convert the Data json object to bytes
+	data, parseErr := json.Marshal(aux.Data)
+	if parseErr != nil {
+		return parseErr
+	}
+
+	event.Data = data
+
+	// no errors
+	return nil
+}
